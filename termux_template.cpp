1317928b58a442634799e25991755d78ac2af25d
tiom4eg <62618821+tiom4eg@users.noreply.github.com>
2024-08-24 12:18:02 +0300

    Create termux_template.cpp

---
+++ termux_template.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <cassert>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <utility>
+#include <bitset>
+#include <tuple>
+#include <array>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <string>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <chrono>
+#include <ctime>
+#include <random>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/hash_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("O3")
+#pragma GCC target("avx,avx2,tune=native")
+#endif
+
+signed main() {
+
+    return 0;
+}
